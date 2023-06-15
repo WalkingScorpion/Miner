3beb1b90bd1b34d2720ca5e304ef2ea7ac225eaa
xuyao <xy_uncertain@163.com>
2023-06-15 10:44:50 +0800

    [Dupdate

--- stock_strategy/x_strategy.py
+++ stock_strategy/x_strategy.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 class XStrategy(object):
-    def __init__(self, history, realtime, sell_base=1.005):
+    def __init__(self, history, realtime, sell_base=0.5):
         self.history = history
         self.realtime = realtime
         self.sb = sell_base
@@ -81,12 +81,12 @@ class XStrategy(object):
         his = his.drop(drop_list)
         info = his['ts_code'][0]
         s = his.shape[0]
-        base_coef = self.sb
+        base_coef = self.sb / 100
         i = 0
         while i < period and 0 <= s - 1 - i:
             index = s - 1 - i
             n = int(str(his['trade_date'][index]).split()[0])
-            sell = base_coef * (1 + i) * price
+            sell = (1 + base_coef * (i + 1)) * price
             if his['high'][index] > sell:
                 return " %d | %f | %f" % (n, sell, sell / price * 100 - 100)
             i += 1
--- stock_trace.py
+++ stock_trace.py
@@ -13,28 +13,30 @@ if __name__=="__main__":
     tuf.fetch_data(offset=1)
     df_his = tuf.dfl
     date = sys.argv[1]
-    print('---- Begin %s Trace ----' % date)
-    with open('data/mock_trade/' + date) as f:
-        c = 0
-        sm = 0.0
-        for line in f:
-            line = line.strip()
-            info = line.split('|')
-            code = info[0].strip()
-            trade_time = info[1].strip()
-            price = float(info[2].strip())
-            rt = rsf.RtSnowballFetcher(code)
-            rt.fetch_data()
-            h = tuf.extract_snapshot(code)
-            r = rt.extract_snapshot(code)
-            s = xs.XStrategy(h, r, 1.008)
-            ret = s.sell_strategy(trade_time.split()[0], price)
-            print('%s  --->  %s' % (line, ret))
-            sp = ret.split('|')
-            if len(sp) > 2:
-                profile = float(ret.split('|')[2])
-                if profile > -9:
-                    sm += profile
-                    c += 1
-        print(sm / c)
-    print('---- All Done %s ----' % date)
+    dl = date.split(',')
+    for d in dl:
+        print('---- Begin %s Trace ----' % d)
+        with open('data/mock_trade/' + d) as f:
+            c = 0
+            sm = 0.0
+            for line in f:
+                line = line.strip()
+                info = line.split('|')
+                code = info[0].strip()
+                trade_time = info[1].strip()
+                price = float(info[2].strip())
+                rt = rsf.RtSnowballFetcher(code)
+                rt.fetch_data()
+                h = tuf.extract_snapshot(code)
+                r = rt.extract_snapshot(code)
+                s = xs.XStrategy(h, r, 0.8)
+                ret = s.sell_strategy(trade_time.split()[0], price)
+                print('%s  --->  %s' % (line, ret))
+                sp = ret.split('|')
+                if len(sp) > 2:
+                    profile = float(ret.split('|')[2])
+                    if profile > -9:
+                        sm += profile
+                        c += 1
+            print(sm / c)
+        print('---- All Done %s ----' % d)
